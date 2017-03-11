c88e50067d7294edc23d87b73bfe4f1ac7dc1940
solgh <solgh@qq.com>
2017-03-11 16:20:22 +0800

    Finish layout and routes

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,10 +1,15 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <meta charset="utf-8">
-    <title>@yield('title','sample-solgh') 二周目</title>
+    <title>@yield('title', 'Sample App') - Laravel 入门教程</title>
+    <link rel="stylesheet" href="/css/app.css">
   </head>
   <body>
-    @yield('content')
+    @include('layouts._header')
+
+    <div class="container">
+      @yield('content')
+      @include('layouts._footer')
+    </div>
   </body>
 </html>
